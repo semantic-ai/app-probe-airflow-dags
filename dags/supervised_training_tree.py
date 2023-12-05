af50e588ff5f0f9058fb2ecd5cb6bc0eee4b8a60
ruben <ruben@ml2grow.com>
2023-12-05 09:55:42 +0100

    feat: lowered cpu usage

--- dags/supervised_training_tree.py
+++ dags/supervised_training_tree.py
@@ -52,7 +52,7 @@ with DAG(
         get_logs=True,
         image_pull_policy="Always",
         startup_timeout_seconds=480,
-        container_resources=k8s.V1ResourceRequirements(limits={"cpu": "8", "memory": "16G"}, requests={"cpu": "4", "memory": "8G"}),
+        container_resources=k8s.V1ResourceRequirements(limits={"cpu": "4", "memory": "16G"}, requests={"cpu": "2", "memory": "8G"}),
         env_vars={
             "RUNS_MODEL_PULL_TOKEN": Variable.get("RUNS_MODEL_PULL_TOKEN"),
             "MLFLOW_TRACKING_URI": Variable.get("MLFLOW_TRACKING_URI"),
